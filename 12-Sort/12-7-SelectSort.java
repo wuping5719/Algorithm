08bd67fa599cc49242b1773ae622736991bc52e2
PingWu <wp571988@163.com>
2016-07-19 23:13:27 +0800

    Create 12-7-SelectSort.java

---
+++ 12-Sort/12-7-SelectSort.java
@@ -0,0 +1,30 @@
+package sort;
+/** 
+ * @author  WuPing
+ * @date 2016年4月2日 下午10:58:52 
+ * @version 1.0 
+ * @parameter  
+ * @since  
+ * @return  
+ */
+public class SelectSort {
+
+	public static void selectSort(int[] array){
+		if(array!=null&&array.length!=0){
+		  int minIndex=0;
+		  for(int i=0; i<array.length-1; i++){ //只需要比较n-1次  
+			 minIndex=i;
+			 for(int j=i+1; j<array.length; j++){  //从i+1开始比较，因为minIndex默认为i了，i就没必要比了。 
+				 if(array[j]<array[minIndex]){
+					minIndex=j;
+				 }
+			 }
+			 if(minIndex!=i){  //如果minIndex不为i，说明找到了更小的值，交换之。  
+				 int temp = array[i];
+				 array[i] = array[minIndex];
+				 array[minIndex] = temp;
+			 } 
+	     }
+	   }
+	}
+}
