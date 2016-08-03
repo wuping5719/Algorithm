01a64f46c2d636a20fe79bd56d9837770e0e7671
PingWu <wp571988@163.com>
2016-08-03 20:35:55 +0800

    Create 2-7-IntegerPackingTest.java

---
+++ 2-Java-Interview/2-7-IntegerPackingTest.java
@@ -0,0 +1,20 @@
+package interview;
+/** 
+ * @author  WuPing
+ * @date 2016年8月3日 下午8:36:14 
+ * @version 1.0 
+ * @parameter  
+ * @since  
+ * @return  
+ */
+public class IntegerPackingTest {
+
+	public static void main(String[] args) {
+		Integer f1 = 100, f2 = 100;
+		Integer f3 = 150, f4 = 150;
+
+        System.out.println(f1 == f2);
+        System.out.println(f3 == f4);
+	}
+
+}
