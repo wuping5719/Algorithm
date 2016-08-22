1adcf96a3bac4b6e9a10e939bec20010a6a97b8c
PingWu <wp571988@163.com>
2016-08-22 22:31:53 +0800

    Create 1-5-1-ListNode.java

---
+++ 1-Sword-Offer/1-5-1-ListNode.java
@@ -0,0 +1,14 @@
+package swordOffer;
+/** 
+ * @author  WuPing
+ * @date 2016年8月22日 下午10:03:17 
+ * @version 1.0 
+ */
+public class ListNode {
+	int val;
+    ListNode next = null;
+    
+    ListNode(int val) {
+        this.val = val;
+    }
+}
