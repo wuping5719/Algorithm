16a2e3b0684a639fa7b08a2e1230fdf4383d765f
PingWu <wp571988@163.com>
2016-08-23 11:08:10 +0800

    Create 1-6-1-TreeNode.java

---
+++ 1-Sword-Offer/1-6-ConstructBinaryTree/1-6-1-TreeNode.java
@@ -0,0 +1,16 @@
+package swordOffer;
+
+/**
+ * @author WuPing
+ * @version 2016年8月23日 上午10:37:42
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+	val = x;
+    }
+}
