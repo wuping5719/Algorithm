dab153fe9969356521fa13f8e7a8f927ad701501
PingWu <wp571988@163.com>
2016-08-24 23:32:36 +0800

    Create 1-26-1-RandomListNode.java

---
+++ 1-Sword-Offer/1-26-CopyComplextList_26/1-26-1-RandomListNode.java
@@ -0,0 +1,16 @@
+package swordOffer;
+/** 
+ * @author  WuPing
+ * @date 2016年8月24日 下午11:06:01 
+ * @version 1.0 
+ */
+
+public class RandomListNode {
+    int val;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int val) {
+        this.val = val;
+    }
+}
