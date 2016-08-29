daded767890c2fd14d347f0c79402a50f302eade
PingWu <wp571988@163.com>
2016-08-29 23:17:06 +0800

    Create 5-9-2-ListNode.java

---
+++ 5-CrackingCodingInterview/5-9-2-ListNode.java
@@ -0,0 +1,15 @@
+package crackingCodingInterview;
+
+/** 
+ * @author  WuPing
+ * @date 2016年8月29日 下午11:16:06 
+ * @version 1.0 
+ */
+public class ListNode {
+	int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
