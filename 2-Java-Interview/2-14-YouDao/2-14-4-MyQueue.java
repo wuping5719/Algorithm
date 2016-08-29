4d95715ed77919621c006482b005f632ea2ecc19
PingWu <wp571988@163.com>
2016-08-29 16:01:56 +0800

    Update 2-14-4-MyQueue.java

--- 2-Java-Interview/2-14-YouDao/2-14-4-MyQueue.java
+++ 2-Java-Interview/2-14-YouDao/2-14-4-MyQueue.java
@@ -1,7 +1,6 @@
 package interview.youDao;
 
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -9,41 +8,30 @@ import java.util.Scanner;
  * @date 2016年8月18日 下午10:26:25
  * @version 1.0
  */
-public class MyQueue {
-	public static void main(String[] args) {
-		Scanner cin = new Scanner(System.in);
-		int t = 0;
-		while (cin.hasNextInt()) {
-			t = cin.nextInt();
-			int n = 0;
-			while (cin.hasNextInt()) {
-				n = cin.nextInt();
-				ConstruteQueue(n);
-			}
-		}
-		cin.close();
-	}
-
-	public static void ConstruteQueue(int n) {
-		Queue<Integer> queue1 = new LinkedList<Integer>();
-		for (int i = 0; i < n; i++) {
-			queue1.offer(i + 1);
-		}
-
-		Queue<Integer> queue2 = new LinkedList<Integer>();
-
-		while (!queue1.isEmpty()) {
-			int x = queue1.peek(); // 取出当前队头的值x
-			queue1.poll(); // 弹出当前队头
-			queue1.offer(x); // 把x放入队尾
-			x = queue1.peek(); // 取出这时候队头的值
-			queue2.add(x);
-			queue1.poll(); // 弹出这时候的队头
-		}
-
-		while (!queue2.isEmpty()) {
-			System.out.print(queue2.poll() + " ");
-		}
-		System.out.println();
-	}
+public class ConstructQueue {
+    public static LinkedList<Integer> constructQueue(int n){
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        for(int i=n; i>=1; i--){
+            list.addFirst(i);
+            list.addFirst(list.removeLast());
+        }
+        return list;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+	int t = scan.nextInt();
+        int n;
+        LinkedList<Integer> result;
+        while(t > 0){
+            t--;
+            n = scan.nextInt();
+            result = constructQueue(n);
+            for(int i=0; i<n-1; i++){
+                System.out.print(result.removeFirst() + " ");
+            }
+            System.out.println(result.removeFirst());
+        }
+        scan.close();
+    }
 }
