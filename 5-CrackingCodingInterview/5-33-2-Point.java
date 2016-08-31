96ef342a2467014e5fb626a2693b1754aee1b205
PingWu <wp571988@163.com>
2016-08-31 22:46:38 +0800

    Create 5-33-2-Point.java

---
+++ 5-CrackingCodingInterview/5-33-2-Point.java
@@ -0,0 +1,21 @@
+package crackingCodingInterview;
+
+/**
+ * @author WuPing
+ * @version 2016年8月31日 下午10:11:46
+ */
+
+public class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+	this.x = x;
+	this.y = y;
+    }
+
+    public Point() {
+	this.x = 0;
+	this.y = 0;
+    }
+}
