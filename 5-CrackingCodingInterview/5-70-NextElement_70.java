c488d1976b0ac694e9bfd3f2bd24c6721fc453ea
PingWu <wp571988@163.com>
2016-10-15 23:35:58 +0800

    Create 5-70-NextElement_70.java

---
+++ 5-CrackingCodingInterview/5-70-NextElement_70.java
@@ -0,0 +1,43 @@
+package crackingCodingInterview;
+
+import java.util.Scanner;
+
+/**
+ * @author WuPing
+ * @version 2016年10月15日 下午6:53:43
+ */
+
+public class NextElement_70 {
+
+    public static int[] findNext(int[] A, int n) {
+        int[] result = new int[n];
+        for(int i=0; i<n; i++) {
+            result[i] = -1;
+        }
+        
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++) {
+                if(A[j] > A[i]) {
+                    result[i] = A[j];
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+    
+    public static void main(String[] args) {
+	Scanner cin = new Scanner(System.in);
+	while (cin.hasNext()) {
+	    int n = cin.nextInt();
+	    int[] A = new int[n];
+	    for(int i=0; i<n; i++) {
+		A[i] = cin.nextInt();
+	    }
+	    
+	}
+	cin.close();
+
+    }
+
+}
