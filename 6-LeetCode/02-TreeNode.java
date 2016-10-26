0d8dc5478e6920e8b6e079d4aa11accd8e74c98b
PingWu <wp571988@163.com>
2016-10-26 21:07:29 +0800

    TreeNode

---
+++ 6-LeetCode/02-TreeNode.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * @author WuPing
+ * @version 2016年10月21日 下午9:39:25
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+	val = x;
+    }
+}
