195b97189983cb1c0a92c1d6fd2f90c12381850b
PingWu <wp571988@163.com>
2016-10-26 21:06:43 +0800

    ListNode

---
+++ 6-LeetCode/01-ListNode.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * @author WuPing
+ * @version 2016年10月21日 下午8:48:41
+ */
+
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int x) {
+	val = x;
+    }
+}
