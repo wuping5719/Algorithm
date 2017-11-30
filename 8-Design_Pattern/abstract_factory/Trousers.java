2819d20d58a4c7ae575c9e3fd6f3fb426f84dc9c
PingWu <wp571988@163.com>
2017-11-30 22:00:08 +0800

    Create Trousers.java

---
+++ 8-Design_Pattern/abstract_factory/Trousers.java
@@ -0,0 +1,7 @@
+package com.myra.design.pattern.abstract_factory;
+
+public abstract class Trousers {
+   public abstract int getWaistSize();
+   public abstract int getHeight();
+   public abstract String getName();
+}
