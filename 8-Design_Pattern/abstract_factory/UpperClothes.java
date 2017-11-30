a2d94b0532e1c2b2e2d7571452da1296551aceae
PingWu <wp571988@163.com>
2017-11-30 22:00:49 +0800

    Create UpperClothes.java

---
+++ 8-Design_Pattern/abstract_factory/UpperClothes.java
@@ -0,0 +1,7 @@
+package com.myra.design.pattern.abstract_factory;
+
+public abstract class UpperClothes {
+    public abstract int getChestSize();
+    public abstract int getHeight();
+    public abstract String getName(); 
+}
