2d1d4e059d1689ec0867ae6cbd54212c212d09fd
PingWu <wp571988@163.com>
2017-12-12 22:21:43 +0800

    Create Flyweight.java

---
+++ 8-Design_Pattern/flyweight/Flyweight.java
@@ -0,0 +1,11 @@
+package com.myra.design.pattern.flyweight;
+
+public interface Flyweight {
+   public double getHeight();
+
+   public double getWidth();
+   
+   public double getLength();
+   
+   public void printMessage(String message);
+}
