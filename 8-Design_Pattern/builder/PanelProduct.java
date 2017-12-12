b850e88fd3847b19a33e21b15de4f9f368b9ed48
PingWu <wp571988@163.com>
2017-12-12 22:07:59 +0800

    Create PanelProduct.java

---
+++ 8-Design_Pattern/builder/PanelProduct.java
@@ -0,0 +1,17 @@
+package com.myra.design.pattern.builder;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PanelProduct extends JPanel {
+	
+	private static final long serialVersionUID = -1668420773900641434L;
+	
+	JButton button;
+	
+	JLabel label;
+	
+	JTextField textField;
+}
