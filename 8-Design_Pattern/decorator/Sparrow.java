48ceebdd6f5ae5ffd08bc243ab0f97b107ade629
PingWu <wp571988@163.com>
2017-12-12 22:16:37 +0800

    Create Sparrow.java

---
+++ 8-Design_Pattern/decorator/Sparrow.java
@@ -0,0 +1,9 @@
+package com.myra.design.pattern.decorator;
+
+public class Sparrow extends Bird {
+    public final int DISTANCE = 100;
+    
+    public int fly() {
+    	return DISTANCE;
+    }
+}
